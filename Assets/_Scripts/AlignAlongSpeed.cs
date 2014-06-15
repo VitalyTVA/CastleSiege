463a16a925953fbc64f76c2fec111fd57d1d0a07
VitalyTVA <vitalytva@yandex.ru>
2014-06-15 20:58:28 +0400

    Align along speed controller

---
+++ Assets/_Scripts/AlignAlongSpeed.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlignAlongSpeed : MonoBehaviour {
+	void FixedUpdate() {
+		if (rigidbody.velocity != Vector3.zero) {
+			rigidbody.MoveRotation(Quaternion.LookRotation(rigidbody.velocity));
+		}
+	}
+}
