441db4d3afb2fb5e1760c27861f0124d9b6ae779
VitalyTVA <vitalytva@yandex.ru>
2014-06-15 23:30:27 +0400

    Shooter distance and target calculation

--- Assets/_Scripts/Shooter.cs
+++ Assets/_Scripts/Shooter.cs
@@ -14,17 +14,18 @@ public class Shooter : MonoBehaviour {
 		while (true) {
 			yield return new WaitForSeconds (1);
 			GameObject shoteeInstance = (GameObject)Instantiate(shotee, transform.position, Quaternion.identity);
-			shoteeInstance.transform.eulerAngles = new Vector3 (-30, 0, 0);
+			//shoteeInstance.transform.eulerAngles = new Vector3 (-30, 0, 0);
 
 
 			float distance = Vector3.Distance(transform.position, target.position);
 			float angle = Mathf.Asin(-Physics.gravity.y * distance / (speed * speed)) / 2;
-
+			var direction = Mathf.Deg2Rad * (Quaternion.LookRotation(transform.position - target.position).eulerAngles.y - 180);
+			Debug.Log((Mathf.Rad2Deg  * direction).ToString());
 			//var angles = shoteeInstance.transform.eulerAngles;
 			//float angle = Mathf.PI * -angles.x / 180;
 
-			var v = new Vector3 (0, Mathf.Sin (angle), Mathf.Cos (angle));
-			shoteeInstance.rigidbody.useGravity = true;
+			var v = new Vector3 (Mathf.Sin(direction) * Mathf.Cos (angle), Mathf.Sin (angle), Mathf.Cos(direction) * Mathf.Cos (angle));
+			//shoteeInstance.rigidbody.useGravity = true;
 			shoteeInstance.rigidbody.AddForce (Vector4.Normalize (v) * speed, ForceMode.VelocityChange);
 		}
 	}
