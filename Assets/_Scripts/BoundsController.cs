442a72b8c6e05096eba9b8d1b4b1b62d9c00774b
VitalyTVA <vitalytva@yandex.ru>
2014-06-26 18:29:53 +0400

    Destroy arrows on out of bounds

---
+++ Assets/_Scripts/BoundsController.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoundsController : MonoBehaviour {
+	void OnTriggerExit(Collider other) {
+		GameObject.Destroy (other.gameObject);
+	}
+}
