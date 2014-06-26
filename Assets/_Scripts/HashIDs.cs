42e8fd5e94b6d971a9ee79d531fe0fa2d9bbda57
VitalyTVA <vitalytva@yandex.ru>
2014-06-26 23:55:52 +0400

    Die animation

--- Assets/_Scripts/HashIDs.cs
+++ Assets/_Scripts/HashIDs.cs
@@ -6,12 +6,17 @@ public class HashIDs : MonoBehaviour
 	// Here we store the hash tags for various strings used in our animators.
 	public int idleState;
 	public int runState;
+	public int dieState;
 	public int speedFloat;
-	
+	public int deadBool;
+
 	void Awake ()
 	{
-		idleState = Animator.StringToHash("Base Layer.idle");
-		runState = Animator.StringToHash("Base Layer.run");
+		idleState = Animator.StringToHash("Base Layer.Idle");
+		runState = Animator.StringToHash("Base Layer.Run");
+		dieState = Animator.StringToHash("Base Layer.Die");
+
 		speedFloat = Animator.StringToHash("Speed");
+		deadBool = Animator.StringToHash("Dead");
 	}
 }
--- Assets/_Scripts/TrollHitableObject.cs
+++ Assets/_Scripts/TrollHitableObject.cs
@@ -2,7 +2,50 @@
 using System.Collections;
 
 public class TrollHitableObject : HitableObjectBase {
+	Animator anim;	
+	HashIDs hash;
+	bool dead = false;
+	bool dying = false;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+		hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+	}
+	
 	public override void Hit() {
+		dying = true;
+//		Animator animator = GetComponent<Animator> ();
+//		HashIDs hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+//		animator.SetBool(hash.deadBool, true);
+
+//		animator.SetBool(hash.deadBool, false);
+		//animator.SetFloat(hash.speedFloat, 0f);
+		//GameObject.Destroy (gameObject, 1f);
 
 	}	
+	void Update ()
+	{
+		if (!dying)
+						return;
+			// ... and if the player is not yet dead...
+		if(!dead)
+			PlayerDying();
+		else
+			PlayerDead();
+	}
+	void PlayerDying ()
+	{
+		dead = true;
+		anim.SetBool(hash.deadBool, dead);
+
+	}
+	
+	
+	void PlayerDead ()
+	{
+		if(anim.GetCurrentAnimatorStateInfo(0).nameHash == hash.dieState)
+			anim.SetBool(hash.deadBool, false);
+		anim.SetFloat(hash.speedFloat, 0f);
+		GameObject.Destroy (gameObject, 1f);
+	}
 }
