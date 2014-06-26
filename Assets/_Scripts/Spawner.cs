2fa199edf1dc5077ef29bcec4f17e4212d9584a2
VitalyTVA <vitalytva@yandex.ru>
2014-06-27 02:22:39 +0400

    block spawner

--- Assets/_Scripts/Spawner.cs
+++ Assets/_Scripts/Spawner.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 public class Spawner : MonoBehaviour {
 	public GameObject spawnee;
 	public Transform target;
-	public float interval = 1;
+	public float interval = 5;
+	public int rows = 2;
+	public int columns = 3;
+	public float distance = 1f;
 
 	void Start () {
 		StartCoroutine (SpawnRoutine ());
@@ -13,8 +16,13 @@ public class Spawner : MonoBehaviour {
 	IEnumerator<WaitForSeconds> SpawnRoutine() {
 		while (true) {
 			yield return new WaitForSeconds (interval);
-			GameObject spawneeInstance = (GameObject)Instantiate(spawnee, transform.position, Quaternion.identity);
-			spawneeInstance.GetComponent<Mover>().target = target;
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					var position = transform.position + new Vector3(j * distance, 0, i * distance);
+					GameObject spawneeInstance = (GameObject)Instantiate(spawnee, position, Quaternion.identity);
+					spawneeInstance.GetComponent<Mover>().target = target;
+				}
+			}
 		}
 	}
 
