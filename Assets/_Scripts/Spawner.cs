8ef45a81d18234c9f09068cda7b97073ddafdb38
VitalyTVA <vitalytva@yandex.ru>
2014-06-26 21:33:49 +0400

    spawner

---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+	public GameObject spawnee;
+	public Transform target;
+	public float interval = 1;
+
+	void Start () {
+		StartCoroutine (SpawnRoutine ());
+	}
+	IEnumerator<WaitForSeconds> SpawnRoutine() {
+		while (true) {
+			yield return new WaitForSeconds (interval);
+			GameObject spawneeInstance = (GameObject)Instantiate(spawnee, transform.position, Quaternion.identity);
+			spawneeInstance.GetComponent<Mover>().target = target;
+		}
+	}
+
+}
