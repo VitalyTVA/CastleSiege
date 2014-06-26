d4f3868dc4cc63057c7e353a850f39c11fda0936
VitalyTVA <vitalytva@yandex.ru>
2014-06-26 21:07:17 +0400

    Create Mover script

---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public Transform target;
+	public float speed = 1f;
+	void Start () {
+//		if (target == null)
+//			return;
+		Animator animator = GetComponent<Animator> ();
+		HashIDs hash = GameObject.FindGameObjectWithTag("GameController").GetComponent<HashIDs>();
+		animator.SetFloat(hash.speedFloat, 1f);
+
+		Vector3 targetDirection = target.transform.position - transform.position;
+		targetDirection.y = 0;
+		rigidbody.velocity = Vector3.Normalize (targetDirection) * speed;
+		
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, 1);
+		rigidbody.MoveRotation(newRotation);
+
+	}
+//	void FixedUpdate() {
+//	}
+}
