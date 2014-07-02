e535f040363d2a7f4f96f428f4a6c019a8d4cbba
VitalyTVA <vitalytva@yandex.ru>
2014-07-03 00:06:58 +0400

    Scene

--- Assets/_Scripts/Helpers/MathHelper.cs
+++ Assets/_Scripts/Helpers/MathHelper.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public static class MathHelper {
 	public static Vector2? SolveQuadratic(float a, float b, float c) {
@@ -9,6 +10,14 @@ public static class MathHelper {
 		return new Vector2 ((-b - Mathf.Sqrt (D)) / (2 * a), (-b + Mathf.Sqrt (D)) / (2 * a));
 	}
 	public static float? CalcShootAngleInRad(float distance, float height, float speed, float gravity) {
+//		var sw = new Stopwatch ();
+//		sw.Start ();
+//		for (int i = 0; i < 100; i++) {
+//			var roots_ = RealPolynomialRootFinder.FindRoots (1, -3.08, -33.9933, 13.223740, 180.9114790);
+//		}
+//		UnityEngine.Debug.Log(sw.ElapsedMilliseconds);
+
+
 		float a = (gravity * distance * distance) / (2 * speed * speed);
 		var roots = SolveQuadratic (a, distance, (a + height));
 		return roots != null ? (float?)Mathf.Atan(roots.Value.Min()) : null;
