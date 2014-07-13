0893a5747ed661ca48d7d1df28e646982dc626e7
VitalyTVA <vitalytva@yandex.ru>
2014-07-13 22:19:00 +0400

    Moving Target Shooting Tests

--- Assets/_Scripts/Tests/TestTarget.cs
+++ Assets/_Scripts/Tests/TestTarget.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class TestTarget : MonoBehaviour {
+    public Vector3 velocity;
+    void Awake() {
+        rigidbody.velocity = velocity;
+    }
 	void OnTriggerEnter(Collider other) {
         transform.parent.GetComponent<TestShooter>().OnHit(gameObject);
     }
